0d3bc8333fee6b4a09d8a67d236b3f027d55a4b4
EYATUPE <eyatuphilipesamu@gmail.com>
2023-04-26 21:58:19 +0300

    Retrieve data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
-"""""""
-This module/Script retrieves information about an employee's TODO list progress from the JSONPlaceholder API.
-"""
-
-import requests
-import sys
-
+""" make a request to a url and extract data """
 
 if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+    import requests
+    from sys import argv
 
-    response = requests.get(url)
-    employeeName = response.json().get('name')
+    emp_id = argv[1]
+    total_todos = 0
+    done_todos = 0
+    done_todo_titles = []
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-    done = 0
-    done_tasks = []
+    result = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                          emp_id)
+    emp_name = result.json().get('name')
 
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
+    result = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                          emp_id + '/todos')
+    emp_todos = result.json()
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
+    for item in emp_todos:
+        total_todos += 1
 
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
+        if item.get('completed') is True:
+            done_todos += 1
+            done_todo_titles.append(item.get('title'))
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        emp_name,
+        done_todos,
+        total_todos
+    ))
+
+    for title in done_todo_titles:
+        print('\t ' + title)
