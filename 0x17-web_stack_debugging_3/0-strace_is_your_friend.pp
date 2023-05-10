0d4cc84cfa5dec2f48df215d2c564fd6ff691eb6
EYATUPE <eyatuphilipesamu@gmail.com>
2023-05-10 12:28:07 +0300

    Fix the typo error on a server

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
-# A puppet script to trance and fix a typo error line in a file on a server
+# A puppet script to trace and fix a typo error line in a file on a server
 $file_root = '/var/www/html/wp-settings.php'
 #replace line containing "phpp" with "php"
-exec { 'replace_line':
+exec { 'edit_line':
   command => "sed -i 's/phpp/php/g' ${file_root}",
   path    => ['/bin','/usr/bin']
 }
