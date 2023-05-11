9075642d86dbe2eaaef45915cf162ad98825c350
EYATUPE <eyatuphilipesamu@gmail.com>
2023-05-12 02:01:50 +0300

    Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0 """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ a function that queries the Reddit API and returns the
+    number of subscribers for a given subreddit """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-agent": "eyatuphilipesamu"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data.get("data").get("subscribers")
+        return (data['data']['subscribers'])
+    else:
+        return 0
