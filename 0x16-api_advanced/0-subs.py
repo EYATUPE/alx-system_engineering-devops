a6a528bab810443d39f11886460c3b6483ea6cc1
EYATUPE <eyatuphilipesamu@gmail.com>
2023-05-12 02:15:28 +0300

    Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,9 @@ def number_of_subscribers(subreddit):
     number of subscribers for a given subreddit """
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-agent": "eyatuphilipesamu"}
+    header_profile = {"User-agent": "eyatuphilipesamu"}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, header_profile=header_profile, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
