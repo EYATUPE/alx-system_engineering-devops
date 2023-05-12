c2f79a0a3a2f42fc3376ddaa286fd5876251f46b
EYATUPE <eyatuphilipesamu@gmail.com>
2023-05-12 12:31:23 +0300

    a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-""" a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit """
+""" a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit
+"""
 
-import requests
+from requests import get
 
 
 def top_ten(subreddit):
-    """  prints the titles of the first 10 hot posts listed for a given subreddit. """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-agent": "eyatuphilipesamu"}
-
-    posts = requests.get(url, headers=headers, allow_redirects=False)
-
-    if posts.status_code == 200:
-        for post in posts.json()['data']['children']:
-            print(post['data']['title'])
+    """  prints the titles of the first 10 hot posts listed for a given subreddit """
+    head = {"User-Agent": "eyatuphilipessamu"}
+    Url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = get(Url, headers=head, allow_redirects=False)
+    posts = response.json()
+    if response.status_code == 200:
+        subscribers = posts.get("data").get("subscribers")
+        for post in subscribers:
+            print(post["data"]["title"])
     else:
-        print(None)
+        print("None")
